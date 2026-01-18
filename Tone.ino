f41c47304c9f0a3039f00b3441595fd992b52dd0
Peter N Andreasen <155552166+oz1ktx@users.noreply.github.com>
2026-01-18 17:09:07 +0100

    Use timer 1 for higher resolution

--- Tone.ino
+++ Tone.ino
@@ -1,11 +1,18 @@
 #include <Wire.h>
 
+const uint8_t num_samples = 100;
+const uint8_t prescaler_1    = 0b001;
+const uint8_t prescaler_8    = 0b010;
+const uint8_t prescaler_64   = 0b011;
+const uint8_t prescaler_256  = 0b100;
+const uint8_t prescaler_1024 = 0b101;
+
 uint8_t counter = 0;
 String frq_str;
 float frequency;
 bool update;
 
-const uint8_t samples[100] = {
+const uint8_t samples[num_samples] = {
   127, 135, 143, 151, 159, 166, 174, 181, 188, 195, 202, 208, 214, 220,
   225, 230, 235, 239, 242, 246, 248, 250, 252, 253, 254, 255, 254, 253,
   252, 250, 248, 246, 242, 239, 235, 230, 225, 220, 214, 208, 202, 195,
@@ -15,13 +22,23 @@ const uint8_t samples[100] = {
   59, 66, 73, 80, 88, 95, 103, 111, 119
 };
 
-uint8_t calc_ocr2a(float frq) {
-  return round( (16E6) / (frq*8) - 1 );
+uint16_t calc_ocr1a(float frq)
+{
+  float result = 16e6 / (frq*1.0*num_samples) - 1;
+  Serial.println(result);
+  return round(result);
 }
 
 void set_frq(float frq) {
-  uint8_t x = calc_ocr2a(frq*100);
-  OCR2A = x;
+  OCR1A = calc_ocr1a(frq);
+  Serial.println(OCR1A);
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+  PORTD = samples[counter++];
+  counter %= num_samples;
+  TCNT1 = 0;
 }
 
 void setup() {
@@ -30,29 +47,17 @@ void setup() {
   //set pins as outputs
   DDRD = B11111111;
   cli();//stop interrupts
-
-  //set timer2 interrupt at 10kHz
-  TCCR2A = 0;// set entire TCCR2A register to 0
-  TCCR2B = 0;// same for TCCR2B
-  TCNT2  = 0;//initialize counter value to 0
-  // set compare match register for frq increments
-  // change to 199 for 10kHz
-  OCR2A = calc_ocr2a(10000); // = (16*10^6) / (10000*8) - 1 (must be <256)
-  // turn on CTC mode
-  TCCR2A |= (1 << WGM21);
-  // Set CS21 bit for 8 prescaler
-  TCCR2B |= (1 << CS21);   
-  // enable timer compare interrupt
-  TIMSK2 |= (1 << OCIE2A);
+  // set timer 1 control register
+  TCCR1B = 0;
+  TCCR1A = 0;
+  TCCR1B |= prescaler_1;
+  TCNT1 = 0;
+  TIMSK1 |= 0b00000010; // set OCIE1A to 1 = enable compare match A
+  // set initial frequency
+  OCR1A = calc_ocr1a(5000);
 
   sei();//allow interrupts
   update = false;
-}
-
-ISR(TIMER2_COMPA_vect) {
-  //timer1 interrupt
-  PORTD = samples[counter++];
-  counter %= 100;
 }
 
 void check_input() {
@@ -61,7 +66,7 @@ void check_input() {
     if (ch == 10 || ch == 13) {
       if (frq_str.length() > 0) {
         float f = atof(frq_str.c_str());
-        if (f>10 && f<30000) {
+        if (f>10 && f<300000) {
           frequency = f;
           frq_str = "";
           update = true;
@@ -69,7 +74,7 @@ void check_input() {
       }
     } else {
       frq_str += ch;
-      if (frq_str.length() > 5) {
+      if (frq_str.length() > 8) {
         frq_str = "";
       }
     }
